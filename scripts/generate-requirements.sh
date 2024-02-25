1e241d8c9404d9f57f952d7c29f662265b868191
Peinan Zhang <peinan7@gmail.com>
2024-02-25 17:48:50 +0900

    remove `-e file:.` in requirements.txt

--- scripts/generate-requirements.sh
+++ scripts/generate-requirements.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cp requirements.lock requirements.txt
+# remove `-e file:.` from requirements.txt because it's not supported by pip-compile
+sed -i '' '/-e file:\./d' requirements.txt
 git add requirements.txt
