90065a66829021077ba6fa27df64f4dba3beea3e
Peinan Zhang <peinan7@gmail.com>
2024-02-25 16:44:33 +0900

    add PDF reader component, avatar, and clear button

--- src/pdfchat/app.py
+++ src/pdfchat/app.py
@@ -1,9 +1,9 @@
-import time
 from dataclasses import dataclass
 from pathlib import Path
 
 import gradio as gr
-from icecream import ic
+from gradio_pdf import PDF
+from loguru import logger
 
 MODEL_CALM2 = "cyberagent/calm2"
 
@@ -56,31 +56,23 @@ def bot(history: ChatHistory, query: str, file_path: str) -> ChatHistory:
         return history
     document = open_file(file_path)
     history.add_chat(Chat(query=query, response=document))
-    ic(history)
+    logger.info(history)
 
     # TODO: use streaming inference
     return history
 
 
 with gr.Blocks() as app:
+    gr.Markdown("# Chat with PDF")
     with gr.Row():
-        with gr.Column(scale=0.4):
+        with gr.Column(scale=35):
             model_name = gr.Dropdown(
                 choices=[MODEL_CALM2],
                 value=MODEL_CALM2,
                 label="Model",
             )
-            file_box = gr.File(
+            file_box = PDF(
                 label="Document",
-                file_types=[".pdf", ".txt"],
-                file_count="single",
-                container=False,
-            )
-            gr.Examples(
-                examples=[["data/sample.txt"], ["data/sample.pdf"]],
-                inputs=[file_box],
-                outputs=[],
-                fn=lambda model_name, document: None,
             )
             with gr.Accordion("Parameters", open=False):
                 temperature_slider = gr.Slider(
@@ -91,24 +83,43 @@ with gr.Blocks() as app:
                     minimum=0.1, maximum=1.0, value=0.5, label="Top P"
                 )
                 top_p_slider.change(lambda x: x, [top_p_slider])
-        with gr.Column(scale=0.6):
+        with gr.Column(scale=65):
             chatbot = gr.Chatbot(
                 bubble_full_width=False,
                 height=650,
+                show_copy_button=True,
+                avatar_images=(
+                    Path("data/avatar-user.png"),
+                    Path("data/avatar-bot.png"),
+                ),
             )
-            ic(chatbot)
+            text_box = gr.Textbox(
+                lines=2,
+                label="Chat message",
+                show_label=False,
+                placeholder="Type your message here",
+                container=False,
+            )
             with gr.Row():
-                text_box = gr.Textbox(
-                    scale=0.9,
-                    show_label=False,
-                    placeholder="Type your message here",
-                    container=False,
+                clear_button = gr.ClearButton(
+                    [text_box, chatbot, file_box], variant="secondary", size="sm"
                 )
-                submit_button = gr.Button("Submit", scale=0.1, variant="primary")
+                submit_button = gr.Button("Submit", variant="primary", size="sm")
                 submit = submit_button.click(
                     fn=bot,
                     inputs=[chatbot, text_box, file_box],
                     outputs=chatbot,
                 )
+    examples = gr.Examples(
+        examples=[
+            [
+                "data/sample.pdf",
+                "胃がん手術の説明書の要点を箇条書きで要約してください",
+            ]
+        ],
+        inputs=[file_box, text_box],
+        outputs=[],
+        fn=lambda model_name, document: None,
+    )
 
 app.queue().launch(debug=True)
